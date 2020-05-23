e114e4a48438e6a9e19ec059f96b80b5d16c3ff2
teesama <44825892+teesama@users.noreply.github.com>
2020-05-23 15:54:28 +0700

    new item
    new item

---
+++ flexbox/detail.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/mycss.css">
+</head>
+<body>
+    <div class="top-bar"></div>
+    <nav class="navbar">
+        <div class="logo-container">
+            <h3 class="logo">Twocars</h3>
+        </div>
+        
+        <div class="link-container left-links-container">
+            <a href="#">ซื้อรถ</a>
+            <a href="#">ขายรถ</a>
+        </div>
+
+        <div class="link-container">
+            <a href="#">Login</a>
+            <a href="#">Register</a>
+        </div>
+    </nav>
+
+    <div class="container">
+        <div class="content-container">
+            <div class="content-left-container">
+                <div class="header-left-container">
+                    <h1> <span class="brand-name">Suzuki : </span><span class="model-name">Equator</span></h1>
+                    <h3 class="price-tag">300,000 บาท</h3>
+                </div>
+
+                <img src="https://source.unsplash.com/featured/?car" class="car-img">
+
+            </div>
+
+            <div class="content-right-container">
+                <div class="header-right-container">
+                    <p>ข้อมูลรถ</p>
+                </div>
+
+                <div class="table-caontainer">
+                    <table class="car-detail-table">
+                        <tr>
+                            <td class="td-left">ประเภทรถ</td>
+                            <td class="td-right">รถมือสอง</td>
+                        </tr>
+                        <tr>
+                            <td class="td-left">ยี่ห้อรถ</td>
+                            <td class="td-right">Suzuki</td>
+                        </tr>
+                        <tr>
+                            <td class="td-left">รุ่น</td>
+                            <td class="td-right">Equator</td>
+                        </tr>
+                        <tr>
+                            <td class="td-left">ชื่อผู้ขาย</td>
+                            <td class="td-right">นายABC</td>
+                        </tr>
+                    </table>
+
+                    <button class="contact-btn">ติดต่อผู้ขาย</button>
+                </div>
+
+            </div>
+
+        </div>
+    
+    <footer>
+        <d class="links-container">
+            <p>2019 Two Cars Ltd. Co.</p>
+            <a href="#">ข้อกำหนด</a>
+            <a href="#">นโยบายส่วนตัว</a>
+        </d>
+
+            <d class="links-container">
+            <a href="#">เกี่ยวกับเรา</a>
+            <a href="#">ช่วยเหลือ</a>
+            <a href="#">คำถามที่พบบ่อย</a>
+        </d>
+    </footer>
+    </div>
+</body>
+</html>
---
+++ flexbox/index.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TwoCars</title>
+    <link rel="stylesheet" href="css/mycss.css">
+</head>
+<body>
+    <div class="top-bar"></div>
+    <nav class="navbar">
+        <div class="logo-container">
+            <h3 class="logo">Twocars</h3>
+        </div>
+
+        <div class="link-container left-links-container">
+            <a href="#">ซื้อรถ</a>
+            <a href="#">ขายรถ</a>
+        </div>
+
+        <div class="link-container">
+            <a href="#">Login</a>
+            <a href="#">Register</a>
+        </div>
+    </nav>
+
+    <div class="hero-container">
+        <h2 class="hero-text-top">ซื้อ - ขายรถมือสอง</h2>
+        <p>ค้นหารถยนต์ใหม่และรถยนต์มือสอง</p>
+
+        <div class="input-container">
+            <input type="text" placeholder="ค้นหาชื่อรุ้นหรือชื่อยี่ห้อ">
+            <button>ค้นหา</button>
+        </div>
+    </div>
+
+    <div class="container">
+        
+        <div class="card-header">
+            <h3>รถมือสองล่าสุด</h3>
+            <a href="#">ดูทั้งหมด</a>
+        </div>
+
+        <div class="cards-container">
+            <div class="card">
+                <img src="https://source.unsplash.com/featured/?car">
+
+                <div class="card-content">
+                    <p class="car-model">Vios</p>
+                    <p class="car-make">Toyota</p>
+                    <p class="car-price">$300,000</p>
+                </div>
+            </div>
+
+            <div class="card">
+                <img src="https://source.unsplash.com/featured/?car">
+
+                <div class="card-content">
+                    <p class="car-model">Vios</p>
+                    <p class="car-make">Toyota</p>
+                    <p class="car-price">$300,000</p>
+                </div>
+            </div>
+
+            <div class="card">
+                <img src="https://source.unsplash.com/featured/?car">
+
+                <div class="card-content">
+                    <p class="car-model">Vios</p>
+                    <p class="car-make">Toyota</p>
+                    <p class="car-price">$300,000</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="card-header">
+            <h3>รถมือสองล่าสุด</h3>
+            <a href="#">ดูทั้งหมด</a>
+        </div>
+
+        <div class="cards-container cards-h-container">
+            <div class="card card-h">
+                <img src="https://source.unsplash.com/featured/?car">
+
+                <div class="card-content">
+                    <p class="car-model">Vios</p>
+                    <p class="car-make">Toyota</p>
+                    <p class="car-price">$300,000</p>
+                </div>
+            </div>
+
+            <div class="card card-h">
+                <img src="https://source.unsplash.com/featured/?car">
+
+                <div class="card-content">
+                    <p class="car-model">Vios</p>
+                    <p class="car-make">Toyota</p>
+                    <p class="car-price">$300,000</p>
+                </div>
+            </div>
+
+            <div class="card card-h">
+                <img src="https://source.unsplash.com/featured/?car">
+
+                <div class="card-content">
+                    <p class="car-model">Vios</p>
+                    <p class="car-make">Toyota</p>
+                    <p class="car-price">$300,000</p>
+                </div>
+            </div>
+            <div class="card card-h">
+                <img src="https://source.unsplash.com/featured/?car">
+
+                <div class="card-content">
+                    <p class="car-model">Vios</p>
+                    <p class="car-make">Toyota</p>
+                    <p class="car-price">$300,000</p>
+                </div>
+            </div>
+
+            <div class="card card-h">
+                <img src="https://source.unsplash.com/featured/?car">
+
+                <div class="card-content">
+                    <p class="car-model">Vios</p>
+                    <p class="car-make">Toyota</p>
+                    <p class="car-price">$300,000</p>
+                </div>
+            </div>
+
+            <div class="card card-h">
+                <img src="https://source.unsplash.com/featured/?car">
+
+                <div class="card-content">
+                    <p class="car-model">Vios</p>
+                    <p class="car-make">Toyota</p>
+                    <p class="car-price">$300,000</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="card-header">
+            <h3>ค้นหายี่ห้อรถ</h3>
+            <a href="#">ดูทั้งหมด</a>
+        </div>
+
+        <div class="logos-container">
+            <img class="logo" src="img/toyota.png" alt="">
+            <img class="logo" src="img/honda.png" alt="">
+            <img class="logo" src="img/mitsubishi.png" alt="">
+            <img class="logo" src="img/nissan.png" alt="">
+            <img class="logo" src="img/mazda.png" alt="">
+        </div>
+
+        <footer>
+            <d class="links-container">
+                <p>2019 Two Cars Ltd. Co.</p>
+                <a href="#">ข้อกำหนด</a>
+                <a href="#">นโยบายส่วนตัว</a>
+            </d>
+
+            <d class="links-container">
+                <a href="#">เกี่ยวกับเรา</a>
+                <a href="#">ช่วยเหลือ</a>
+                <a href="#">คำถามที่พบบ่อย</a>
+            </d>
+        </footer>
+    </div>
+</body>
+</html>
